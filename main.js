61bb8be85618adaa2ab490d07e1470d0847c779b
chysis <chysiss@naver.com>
2022-03-26 22:53:48 +0900

    backup

---
+++ main.js
@@ -0,0 +1,4 @@
+const contents = document.querySelector('.contents');
+const gallery = document.querySelector('.howToView .buttons .galleryView');
+const list = document.querySelector('.howToView .buttons .listView');
+
