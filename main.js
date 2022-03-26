acbadf4fa37b8ae3242e277365e777a8190eedbe
chysis <chysiss@naver.com>
2022-03-26 23:33:52 +0900

    first day ended.

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ const contents = document.querySelector('.contents');
 const gallery = document.querySelector('.howToView .buttons .galleryView');
 const list = document.querySelector('.howToView .buttons .listView');
 
+gallery.addEventListener('click', ()=>{
+    contents.classList.add('galleryOnly');
+    contents.classList.remove('listOnly');
+});
+
+list.addEventListener('click', ()=>{
+    contents.classList.add('listOnly');
+    contents.classList.remove('galleryOnly');
+})
